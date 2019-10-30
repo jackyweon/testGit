7d2106e869a358cf8a8770640e8b7eb90d23c305
wenlei <17313182331@189.cn>
2019-10-30 10:22:07 +0800

    test git commit

---
+++ testGit1/src/main/java/testGit1/test/Test1.java
@@ -0,0 +1,15 @@
+package testGit1.test;
+
+/**
+ * 
+ *
+ * @title: Test1
+ * @company: sctele
+ * @author wenlei
+ * @date 2019年10月30日
+ */
+public class Test1 {
+
+}
+
+
---
+++ testGit2/src/main/java/testGit2/test/test2.java
@@ -0,0 +1,15 @@
+package testGit2.test;
+
+/**
+ * 
+ *
+ * @title: test2
+ * @company: sctele
+ * @author wenlei
+ * @date 2019年10月30日
+ */
+public class test2 {
+
+}
+
+
